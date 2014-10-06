4148952c30477577a642525efe9aa1b06e54fe45
Marcel Martin <marcel.martin@scilifelab.se>
2014-10-06 23:12:42 +0200

    remove unused code; document what needs to be done

--- scripts/phasedblocks.py
+++ scripts/phasedblocks.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 """
-Read a VCF file and output a BED file that shows phased blocks.
+Read a VCF file and annotate phased blocks.
 
-Only the first sample is considered.
+* Statistics are written to stderr (no. of blocks,
+  average length of blocks, etc.)
 
-Only SNPs are considered (indels are treated as if they did not exist).
-Homozygous calls are also ignored.
+* A GTF file is created which describes the location of phased blocks.
+  The file can be loaded into IGV. Since the blocks are
+  not necessarily contiguous, a block can be made up of
+  multiple "exons".
 
-TODO make sure this works when there's more than one chromosome in the VCF
+TODO
+* The GTF is always written to 'out.gtf'.
+* Only the first sample is considered.
+* Only SNPs are considered (indels are treated as if they did not exist).
+* Homozygous calls are also ignored.
+* make sure this works when there's more than one chromosome in the VCF
+* factor out common code of parse_hp_tags and parse_pipe_notation
 """
-import argparse
-from collections import Counter
 import logging
 import sys
-
+from collections import Counter
+import vcf
 from sqt import HelpfulArgumentParser
 from sqt.math import frequency_median
-import vcf
 
 __author__ = 'Marcel Martin'
 
@@ -176,55 +183,6 @@ def parse_hp_tags(path):
 	printe('Largest block:    ', max(blocks.values()))
 	printe('Median block size:', sorted(blocks.values())[len(blocks) // 2])
 	printe('Smallest block:   ', min(blocks.values()))
-
-
-
-def nothing():
-	if False:
-		# Look for blocks of phased variants. According to the VCF specification,
-		# the first variant in such blocks is marked as 'unphased'. Since it
-		# needs to be considered part of the phased block, we keep track of the
-		# position of the previous variant in prev_start.
-		if call.phased:
-			assert n > 0, "First variant in VCF is phased. This shouldn't be."
-			# block_start is None when we're outside a block
-			if block_start is None:
-				# start a new block, but start it at the position of the
-				# previous variant
-				block_start = prev_start
-				n_variants = 2  # this one and the one at prev_start
-			else:
-				n_variants += 1
-		else:
-			if block_start is not None:
-				# end of block
-				assert prev_start > block_start
-				block_length = prev_start - block_start + 1
-				print(record.CHROM, block_start, prev_start + 1, 'Len={},N={}'.format(block_length, n_variants), sep='\t')
-				block_lengths.append(block_length)
-				block_start = None
-			else:
-				singletons += 1
-
-		prev_start = record.start
-		n += 1
-	if block_start is not None:
-		assert prev_start > block_start
-		block_length = prev_start - block_start + 1
-		print(record.CHROM, block_start, prev_start + 1, 'Len={},N={}'.format(block_length, n_variants), sep='\t')
-		block_lengths.append(block_length)
-
-	# print statistics
-	def printe(*args, **kwargs):
-		kwargs['file'] = sys.stderr
-		print(*args, **kwargs)
-
-	printe('considered variants:', n)
-	printe('blocks:', len(block_lengths))
-	printe('longest block:', max(block_lengths))
-	printe('shortest block:', min(block_lengths))
-	printe('average block: {:.2f}'.format(sum(block_lengths) / len(block_lengths)))
-	printe('singletons (unphased variants not within a block):', singletons)
 
 
 def main():
