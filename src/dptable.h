c695741c658fc568738b7012540988d6edbb8c9e
Tobias Marschall <tobias.marschall@0ohm.net>
2014-11-14 10:12:29 +0100

    Remove get_raw_haplotype and get_haplotype (unecessary, special cases of get_super_reads). Remove HALF_TABLE stuff, not used right now, if we use it, it should REPLACE the other code.

--- src/dptable.h
+++ src/dptable.h
@@ -37,26 +37,12 @@ public:
   
   unsigned int get_optimal_score();
   
-  // returns the haplotype for the reads (needs the input file again)
-  typedef std::vector<std::vector<Entry::allele_t> > haplotype_t;
-  std::auto_ptr<haplotype_t> get_haplotype(ColumnIterator * column_iterator);
-  std::auto_ptr<haplotype_t> get_raw_haplotype(ColumnIterator * column_iterator);
-
   // returns the two super-reads for the reads (see above)
   typedef std::vector<std::vector<Entry *> > read_t;
   std::auto_ptr<read_t> get_super_reads(ColumnIterator * column_iterator);
 
   // returns optimal partitioning of the reads
   std::auto_ptr<std::vector<bool> > get_optimal_partitioning();
-  
-  // HALF_TABLE stuff ... when the half-table idea pans out
-
-  // helper function to compute the optimal path through the backtrace table
-  std::auto_ptr<std::vector<unsigned int> > get_index_path_HALF_TABLE();
-
-  // returns the haplotype for the reads (needs the file again)
-  std::auto_ptr<haplotype_t> get_haplotype_HALF_TABLE(ColumnIterator & column_iterator);
-
 };
 
 #endif
