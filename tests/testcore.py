eb74215ad5e963c3a91fc674a71793b42c654b3c
Marcel Martin <marcel.martin@scilifelab.se>
2014-12-23 23:52:42 +0100

    More tests for ReadSet and DPTable

--- tests/testcore.py
+++ tests/testcore.py
@@ -1,6 +1,18 @@
-from whatshap.core import Read
+from whatshap.core import Read, DPTable, ReadSet
 
 def test_read():
 	r = Read("name", 15)
 	assert r.getName() == "name"
 	assert r.getMapqs()[0] == 15
+
+
+def test_readset():
+	rs = ReadSet()
+	assert len(rs) == 0
+
+
+def test_empty_readset():
+	rs = ReadSet()
+	# TODO Segfault in the following line
+	dp_table = DPTable(rs, all_heterozygous=False)
+	superreads = dp_table.getSuperReads()
