56a6d6ad45b894171810f9dd246bbf5b23462d86
Marcel Martin <marcel.martin@scilifelab.se>
2015-01-03 18:28:11 +0100

    Another test for ReadSet

--- tests/testcore.py
+++ tests/testcore.py
@@ -6,12 +6,33 @@ def test_read():
 	assert r.getMapqs()[0] == 15
 
 
-def test_readset():
+def test_empty_readset():
 	rs = ReadSet()
 	assert len(rs) == 0
 
 
-def test_empty_readset():
+def test_readset():
+	rs = ReadSet()
+	r = Read('Read A', 56)
+	r.addVariant(100, 'A', 1, 37)
+	r.addVariant(101, 'C', 0, 18)
+	rs.add(r)
+
+	# TODO This entry is automatically filtered out when added to the ReadSet.
+	r = Read('Read B', 0)
+	r.addVariant(100, 'C', 0, 23)
+	rs.add(r)
+
+	r = Read('Read C', 17)
+	r.addVariant(102, 'G', 1, 27)
+	r.addVariant(105, 'T', 0, 14)
+	rs.add(r)
+
+	rs.finalize()
+	assert len(rs) == 2
+
+
+def test_phase_empty_readset():
 	rs = ReadSet()
 	rs.finalize()
 	dp_table = DPTable(rs, all_heterozygous=False)
