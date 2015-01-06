d262f6c02375db3f3c37d9e47c0a38e2d1baa4f4
Tobias Marschall <tobias.marschall@0ohm.net>
2015-01-06 18:25:47 +0100

    Extended test_readset()

--- tests/testcore.py
+++ tests/testcore.py
@@ -19,16 +19,46 @@ def test_readset():
 	rs.add(r)
 
 	r = Read('Read B', 0)
-	r.addVariant(100, 'C', 0, 23)
+	r.addVariant(101, 'C', 0, 23)
 	rs.add(r)
 
 	r = Read('Read C', 17)
-	r.addVariant(102, 'G', 1, 27)
+	r.addVariant(99, 'G', 1, 27)
 	r.addVariant(105, 'T', 0, 14)
 	rs.add(r)
 
+	assert rs[0].getName() == 'Read A'
+	assert rs[1].getName() == 'Read B'
+	assert rs[2].getName() == 'Read C'
+
 	rs.finalize()
+
+	# should be sorted after finalization
+	assert rs[0].getName() == 'Read C'
+	assert rs[1].getName() == 'Read A'
+	assert rs[2].getName() == 'Read B'
+
 	assert len(rs) == 3
+
+	assert rs.getPositions() == [99,100,101,105]
+
+	r = rs.getByName('Read A')
+	assert r.getName() == 'Read A'
+	assert r.getMapqs() == (56,), str(r.getMapqs())
+
+	r = rs.getByName('Read B')
+	assert r.getName() == 'Read B'
+	assert r.getMapqs() == (0,)
+
+	try:
+		# Should raise a KeyError for non-existing read name
+		r = rs.getByName('foo')
+		assert False
+	except KeyError:
+		pass
+
+	# TODO: Test subset method
+
 
 
 def test_phase_empty_readset():
