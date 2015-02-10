83ce7f4526b2f88300084a7be65540459d348160
saorfi <fischer.sarah.oranna@gmail.com>
2015-02-10 14:26:41 +0100

    working priorityqueue and tests for priority queue

--- tests/testpriorityqueue.py
+++ tests/testpriorityqueue.py
@@ -12,6 +12,19 @@ def test_queue():
 	assert pq.pop() == (5, 'b')
 	assert pq.pop() == (3, 'd')
 
+def test_queue2():
+	pq = PriorityQueue()
+	pq.push(1, 'a')
+	pq.push(50, 'b')
+	pq.push(2, 'c')
+	pq.push(30, 'd')
+	assert len(pq) == 4
+	assert pq.pop() == (50, 'b')
+	assert pq.pop() == (30, 'd')
+	assert pq.pop() == (2, 'c')
+	assert pq.pop() == (1, 'a')
+
+
 def test_change_score():
 	pq = PriorityQueue()
 	pq.push(10, 'a')
@@ -27,7 +40,45 @@ def test_change_score():
 	assert pq.pop() == (2, 'a')
 	assert pq.pop() == (1, 'c')
 
+def test_change_score_sorting():
+	pq=PriorityQueue()
+	pq.push(50,'1')
+	pq.push(40,'2')
+	pq.push(30,'3')
+	pq.push(20,'4')
+	pq.push(10,'5')
+	pq.change_score('5',100)
+	pq.change_score('2',45)
+	assert len(pq) == 5
+	assert pq.pop() == (100,'5')
+	#assert pq.pop() == (45,'2')
+	pq.push(60,'8')
+	assert pq.pop() == (60,'8')
+	pq.change_score('2',40)
+	assert pq.pop()== (50,'1')
+	assert pq.pop() == (40,'2')
 
+
+def test_read_construction():
+	pq=PriorityQueue()
+	pq.push(2,(1,(0,1)))
+	pq.push(2,(2,(0,1)))
+	pq.push(4,(5,(0,1,2,3)))
+	pq.push(3,(6,(0,1,2)))
+	pq.push(4,(7,(0,1,2,3)))
+	pq.push(2,(3,(1,2)))
+	pq.push(3,(4,(1,2,3)))
+	assert len(pq) == 7
+	pq.pop()
+	pq.pop()
+	assert len(pq) == 5
+	pq.change_score((4,(1,2,3)),1)
+	assert pq.pop()== (3,(6,(0,1,2)))
+
+
+
+
+'''
 def test_construction():
 	pq=PriorityQueue()
 
@@ -48,3 +99,4 @@ def test_comparison():
 
 
 	assert pq.getscore(1)== 50
+'''
--- whatshap/priorityqueue.py
+++ whatshap/priorityqueue.py
@@ -1,5 +1,6 @@
 import math
 
+#TODO : Important to use tuples instead of lists of SNPs because llist are unhasable for the dictionary
 
 class PriorityQueue:
 	def __init__(self):
@@ -11,76 +12,106 @@ class PriorityQueue:
 	def push(self, score, item):
 		'''Add item with given score to the heap.'''
 		# Item stored with score in a tupel where the score is always the first position..
-		# insert item in the last position of the heap and then change the position till it suits
-		newindex = len(self.heap) + 1
+		newindex = len(self.heap)
 		self.heap.insert(newindex, (score, item))
+		print('ITEM')
+		print(item)
 		self.positions[item] = newindex
 		self.parent_check_and_swap(newindex)
 
-		#if self.parent_check_and_swap(newindex):
-		#	continue
-		#else:
-		#	print('Pushing does not work')
-
-
-		#TODO not sure if heapify in this case needed
-		#self.heapify(newindex)
+		#TODO Need to look to children not necessary if only pushed to parent.
+		#self.child_check_and_swap(newindex)
 
 
 
 	def swap(self, f_index, s_index):
 		'''swaps the position of the nodes in the priority queue from first index(f_index)and second index(s_index)'''
+
 		firstitem = self.heap[f_index]
 		firstpos = self.positions[self.getitem(f_index)]
-		self.heap[f_index] = self.heap[s_index]
-		self.positions[self.getitem(f_index)] = self.positions[self.getitem(s_index)]
+
+		seconditem = self.heap[s_index]
+		secpos= self.positions[self.getitem(s_index)]
+
+		self.positions[self.getitem(f_index)] =secpos
+		self.positions[self.getitem(s_index)] =firstpos
+
+		self.heap[f_index] = seconditem
 		self.heap[s_index] = firstitem
-		self.positions[self.getitem(s_index)] = firstpos
 
 
 	def parent_check_and_swap(self, index):
 		''' Check if score of item at given index is higher than the score of the parent and swaps the nodes till priority
 		queue property is restored'''
 		parentindex = self.hparent(index)
-		if self.getscore(parentindex) < self.getscore(index):
-			self.swap(parentindex, index)
-			# Look if that works with the Index after swapping
-			self.parent_check_and_swap(parentindex)
-		#else:
-		#	return
+		if parentindex!= index and parentindex >=0 :
+			if self.getscore(parentindex) < self.getscore(index) :
+
+				self.swap(parentindex, index)
+				self.parent_check_and_swap(parentindex)
+
 
 	def child_check_and_swap(self,index):
+		'''Check if score of item at given index is lower than the score of its children, therefore need to swap position with
+		children'''
+		rchildindex=self.hright(index)
+		lchildindex=self.hleft(index)
+		if rchildindex!= index and rchildindex < len(self.heap):
+			if self.getscore(rchildindex)> self.getscore(index):
+				self.swap(rchildindex,index)
+				self.child_check_and_swap(rchildindex)
+		if lchildindex != index and lchildindex < len(self.heap):
+			if self.getscore(lchildindex)> self.getscore(index):
+				self.swap(lchildindex,index)
+				self.child_check_and_swap(lchildindex)
 
 
 
 	def pop(self):
 		'''Removes the item with largest score and returns it as (score, item).'''
-		largest_element = self.extract_max()
-		largest_element = self.heap[0]
-		# Not sure if this works (TODO TEST)
-		self.positions.pop(largest_element)
+		#Gives maximal item in the priority queue , erases it from the queue and updates the scores and indices'''
 
-		return self.heap[0]
-		pass
+		#looks if heap is only one element then no need restore heap property
+		if len(self.heap)>1:
+			#remember last element, max element and their  items
+			latest_element=self.heap[len(self.heap)-1]
+
+			item_latest=self.getitem(len(self.heap)-1)
+			item_max=self.getitem(0)
+
+			ma_element =self.heap.pop(0)
+
+			self.heap.pop(len(self.heap)-1)
+			self.heap.insert(0,latest_element)
+
+			self.positions[item_latest]= 0
+			self.positions.pop(item_max)
+
+			self.child_check_and_swap(0)
+
+		else:
+			self.positions.pop(self.getitem(0))
+			ma_element=self.heap.pop(0)
+
+		return ma_element
+
+
 
 
 	def change_score(self, item, new_score):
 		'''Changes the score of the given item.'''
 		position=self.positions[item]
 		old_score= self.getscore(position)
+		self.heap.pop(position)
 		self.heap.insert(position,(new_score,item))
+
+		# Differentiate between increasing and decreasing score
 		if old_score<new_score:
 			self.parent_check_and_swap(position)
 		else:
 			self.child_check_and_swap(position)
 
-				# Differentiate between higher or lower values in order to compare to the parent or to the children....
 
-		# TODO: to implement that, one could use a dictionary mapping items to its position in the heap
-
-
-
-# TODO do not know if this method is required.
 	def getscore(self, index):
 		'''Return the score of the element at this index in the heap'''
 		(score, item) = self.heap[index]
@@ -98,43 +129,11 @@ class PriorityQueue:
 
 
 	def isEmpty(self):
-		'''Return if sctual Priority Queue is Empty'''
+		'''Return if actual Priority Queue is Empty'''
 		if len(self) == 0:
 			return True
 		else:
 			return False
-
-
-	def heapify(self, index):
-		'''Restores the max priority queue property starting at the given index'''
-		l = self.hleft(index)
-		r = self.hright(index)
-
-		if l <= (len(self) - 1) and self.getscore(l) > self.getscore(index):
-			largest = l
-		else:
-			largest = index
-		# NOT sure if small or like (in book smaller equal)
-		if r < len(self) and self.getscore(r) > self.getscore(largest):
-			largest = r
-		if largest != index:
-			#change the index and largest node in the heap
-			help = self.heap[index]
-			helppos = self.positions[index]
-			self.heap[index] = self.heap[largest]
-			self.positions[index] = self.positions[largest]
-			self.heap[largest] = help
-			self.positions[largest] = helppos
-
-			self.heapify(largest)
-
-
-	def extract_max(self):
-		'''Gives maximal item in the priority queue , erases it from the queue and updates the scores and indices'''
-
-
-	def compare(self, item1, item2):
-
 
 	# Indexes of parent, left and right child maybe also coded by bitshifting
 
@@ -151,78 +150,5 @@ class PriorityQueue:
 	def hright(self, index):
 		'''return the index of the right child in the heap depending on the given index'''
 		return (math.ceil(2 * index) + 2)
-
-
-	#def extract_Element(self,H,read):
-	#	H.remove(read)
-	#	return 0
-
-
-
-	#Not needed if we push each element for itself...
-	def create_new_heap(self, A):
-		heapsize = self.size(A)
-		index = math.floor((heapsize / 2)) - 1
-		while index >= 0:
-			self.max_new_heapify(A, index)
-			index -= 1
-		return A
-
-
-	#create a heap out of given array of elements
-	def max_new_heapify(self, A, i):
-		l = self.hleft(i, A)
-		r = self.hright(i, A)
-		#TODO STILL NOT SURE IF INDEX IS REALLY RIGHT....
-		if l <= (self.size(A) - 1) and A[l][1] > A[i][1]:
-			largest = l
-		else:
-			largest = i
-		#NOT sure if small or like (in book smaller equal)
-		if r < self.size(A) and A[r][1] > A[largest][1]:
-			largest = r
-		if largest != i:
-			help = A[i]
-			A[i] = A[largest]
-			A[largest] = help
-			self.max_new_heapify(A, largest)
-
-
-	'''
-		#removing the root node of a max-heap, AND decrease the score of the other reads included in the SNPS covered by this read by 1
-		def delete_max(self, H):
-			max=self.Extract_new_Max(H)
-			#TODO MAYBE INCLUDED INTO EXTRACT MAX
-			#covered_variants = self.Variants_of_Max
-			#for k in covered_variant.getReads :
-			#	deccrease_key (k)
-			#	heapify Reads (index ....)
-			h = 0
-			return h
-		'''
-
-
-
-
-
-
-
-
-
-	#decreases the scores of all reads in the heap
-	def Shift_down(self, H):
-		#dont want negative scores
-		for i in range(0, len(H)):
-			if H[i][1] != 0:
-				H[i][1] -= 1
-			#else :
-			#print('Score already below 0')
-			#Nothing to be done only for information...
-			#TODO better solution .... Remove node out of HEAP ?
-
-		return 0
-
-
-
 
 
