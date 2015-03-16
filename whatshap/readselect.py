e92f587526e94106301849756167fda077913a59
saorfi <fischer.sarah.oranna@gmail.com>
2015-03-16 10:53:35 +0100

    working read slicing with beginning development of component finding

--- whatshap/readselect.py
+++ whatshap/readselect.py
@@ -20,6 +20,7 @@ from whatshap._core import PyIndexSet as IndexSet
 #from whatshap.scripts.whatshap import CoverageMonitor as CovMonitor
 from whatshap.priorityqueue import PriorityQueue
 from whatshap.coverage import CovMonitor
+from .graph import ComponentFinder
 
 #TODO Need to assert somewhere that if less Reads than coverage...?
 
@@ -72,20 +73,22 @@ def slice_read_selection(pq,coverages,already_covered_SNPS,selected_reads,phasab
 	'''Extraction of a set of read indices, where each SNP should be covered at least once, if coverage, or reads are allowing it '''
 	#reads selected in this run
 	#actual_selection=[]
-
+	#Intern list for storing the actual selected reads
+	intern_list=[]
 	#TODO add an additional condition like if number covered SNPS equal phasable SNPS
 	while len(pq)!= 0:
 		#TODO ISEMPTY does not work
 
+
 		(max_score,max_item)= pq.pop()
 
 		if not max_item in selected_reads:
+
 			extracted_read=readset[max_item]
 			uncovered_SNP= False
 			#look if positions covered by this reads are already covered or not
 			for pos in extracted_read:
-				#TODO could be done easier
-				if pos in already_covered_SNPS:
+				if pos.position  in already_covered_SNPS:
 					continue
 				else:
 					uncovered_SNP=True
@@ -97,10 +100,12 @@ def slice_read_selection(pq,coverages,already_covered_SNPS,selected_reads,phasab
 			begin=Vcf_indices.get(extracted_read[0].position)
 			end= Vcf_indices.get(extracted_read[len(extracted_read)-1].position)
 
+
 			if uncovered_SNP and coverages.max_coverage_in_range(begin,end)<MAX_cvo:
 				coverages.add_read(begin,end)
 				#selected_reads includes only the read indices of the selected reads ....
 				selected_reads.append(max_item)
+				intern_list.append(max_item)
 
 				#again go over the positions in the read and add them to the already_covered_SNP list
 				for pos in extracted_read:
@@ -123,10 +128,164 @@ def slice_read_selection(pq,coverages,already_covered_SNPS,selected_reads,phasab
 						oldscore=pq.get_score_by_item(element)
 						if oldscore != None:
 							pq.change_score(element,oldscore-1)
-						else:
-							print('Current element not anymore member of the priority queue  ')
+	return  (intern_list,selected_reads,coverages)
 
-	return  (selected_reads,coverages)
+
+def create_new_readset(readset,erasing_indices):
+	'''returns given a readset and a list of indices which are to erased out od the readset and returns the corresponding IndexSet'''
+	new_set= IndexSet()
+	readset_indices= set (i for i in range(0,len(readset)))
+	for j in readset_indices:
+		if j not in erasing_indices:
+			new_set.add(j)
+	return new_set
+
+
+#TODO Setting up another readset does not work, maybe not needed
+def building_up_final_readset(new_readset_subset,sliced_selected_reads,final_selected_reads,original_readset):
+	'''out of a given Readset (new_readset_subset) and selected indices, build up a readset in final_selected_reads'''
+	#for i in sliced_selected_reads:
+	#	read= new_readset_subset[i]
+	#
+	#	print('Found read')
+	#	print(read)
+	#	final_selected_reads.push(read)
+	#print('final_selected_reads')
+	#print(final_selected_reads)
+	return final_selected_reads
+
+
+def find_new_blocks(actual_reads,all_possible_reads,component_finder):
+
+
+
+	return (component_finder,components)
+
+
+def init_blocking(actual_reads, former_selected_reads,vcf_indices,reads):
+	all_possible_positions= vcf_indices.keys()
+	#initializations done elsewhere because here it should be iteratively adding the new found reads into the already
+	#existing component finder data structure....
+	component_finder=ComponentFinder(all_possible_positions)
+
+	print('component_finder')
+	#print(component_finder)
+	print('VCF positions ')
+	#print(vcf_indices.keys())
+	for i in actual_reads:
+		read= reads[i]
+		#same code as in find_components....
+		#need here a list and not a se with {}
+		read_positions= [variant.position for variant in read if variant.position in all_possible_positions]
+		print('Read positions')
+		print(read_positions)
+		for position in read_positions[1:]:
+			component_finder.merge(read_positions[0], position)
+
+		#print(reads[i])
+	components={position : component_finder.find(position) for position in all_possible_positions}
+	print('Components')
+	print(components)
+
+	#TODO it does not work here because a component _finder is always new initialized....
+
+
+	print('Here')
+	return (component_finder,all_possible_positions)
+
+
+
+
+
+
+
+
+
+#How the componentfind works....
+'''
+def find_components(superreads, reads):
+	"""
+	Return a dict that maps each position to the component it is in. A
+	component is identified by the position of its leftmost variant.
+	"""
+	logger.debug('Finding connected components ...')
+
+	# The variant.allele attribute can be either 0 (major allele), 1 (minor allele),
+	# or 3 (equal scores). If all_heterozygous is on (default), we can get
+	# the combinations 0/1, 1/0 and 3/3 (the latter means: unphased).
+	# If all_heterozygous is off, we can also get all other combinations.
+	# In both cases, we are interested only in 0/1 and 1/0.
+	phased_positions = [ v1.position for v1, v2 in zip(*superreads)
+		if (v1.allele, v2.allele) in ((0, 1), (1, 0))
+	]
+	assert phased_positions == sorted(phased_positions)
+
+	# Find connected components.
+	# A component is identified by the position of its leftmost variant.
+	component_finder = ComponentFinder(phased_positions)
+	phased_positions = set(phased_positions)
+	for read in reads:
+		positions = [ variant.position for variant in read if variant.position in phased_positions ]
+		for position in positions[1:]:
+			component_finder.merge(positions[0], position)
+	components = { position : component_finder.find(position) for position in phased_positions }
+	logger.info('No. of variants considered for phasing: %d', len(superreads[0]))
+	logger.info('No. of variants that were phased: %d', len(phased_positions))
+	return components
+
+def best_case_blocks(reads):
+	"""
+	Given a list of core reads, determine the number of phased blocks that
+	would result if each variant were actually phased.
+
+	Return the number of connected components.
+	"""
+	positions = set()
+	for read in reads:
+		for variant in read:
+			positions.add(variant.position)
+	component_finder = ComponentFinder(positions)
+	for read in reads:
+		read_positions = [ variant.position for variant in read ]
+		for position in read_positions[1:]:
+			component_finder.merge(read_positions[0], position)
+	# A dict that maps each position to the component it is in.
+	components = { component_finder.find(position) for position in positions }
+	return len(components)
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def readselection(readset, positions,max_cov):
@@ -146,57 +305,82 @@ def readselection(readset, positions,max_cov):
 	coverages = CovMonitor(len(positions))
 
 	#TODO Look if we could also use there a set instead of an array
-	selected_reads= []
-	#Use as set
-	already_covered_SNPs=set()
 
-
-	(sliced_selected_reads,coverages)= slice_read_selection(pq,coverages,already_covered_SNPs,selected_reads,phasbale_SNPs,max_cov,readset,vcf_indices,SNP_read_map)
-	print('selected_reads')
-	print(selected_reads)
-	new_set=readset
-	print(readset)
-	print(len(readset))
-	new_read_index_set= [i for i in range(0,len(readset))]
-	print('new_read_index_set')
-	print(new_read_index_set)
-	#Now intersection
-
-	#(sliced_selected_reads,coverages)= slice_read_selection(pq,coverages,already_covered_SNPs,selected_reads,phasbale_SNPs,max_cov,readset,vcf_indices,SNP_read_map)
-
-
-
-
-
-
-
-
+	final_selected_reads=ReadSet()
 
 	#gives only one sclice of the reads (so where every read should be covered at least once)
 	loop_integer= max_cov
-	#while loop_integer!=0 :
+
+	new_readset_subset=readset
+	#TODO Look if we could also use there a set instead of an array
+	selected_reads= []
+
+	final_read_set= set()
+
+
+
+	#Loop where the slicing read selection and the corresponding bridging is done
+
+
+	#TODO - Has to know that the indices are shifting now, because the readset has differed....
+	while loop_integer!=0 :
 	#	#TODO have to do :
 	#	#slice the reads
 	#	#do the block finding
-	#	#build up a new pq with the readset without the selected reads
+	#	#build up a new pq with the readset without the selected reads TODO maybe not needed to minimize the qp?
 	#
-	#
-	#	(sliced_selected_reads,coverages)= slice_read_selection(pq,coverages,already_covered_SNPs,selected_reads,phasbale_SNPs,max_cov,readset,vcf_indices,SNP_read_map)
-	#	loop_integer -=1
 
+		# #should always be an empty set, because for each slice it should start new
+
+		already_covered_SNPs=set()
+		former_selected_reads= selected_reads
+		#TODO have to differ between REads added only in this round and all reads till there but has to be done in slice_read_selection
+		(actual_reads,selected_reads,coverages)= slice_read_selection(pq,coverages,already_covered_SNPs,selected_reads,phasbale_SNPs,max_cov,new_readset_subset,vcf_indices,SNP_read_map)
+		#TODO maybe not needed because of checkout what was already selected
+		#final_selected_reads=building_up_final_readset(new_readset_subset,sliced_selected_reads,final_selected_reads,original_readset)
+
+		#print('actual reads')
+		#print(actual_reads)
+
+		for slice in selected_reads:
+			final_read_set.add(slice)
+		#print('SELECTED READS')
+		#print(selected_reads)
+		#TODO here do the bridging
+
+		(component_finder,all_possible_positions)=init_blocking(actual_reads,former_selected_reads,vcf_indices,readset)
+		(component_finder,components)=find_new_blocks(actual_reads,all_possible_positions,component_finder)
+
+		print('Components')
+		print(components)
+
+
+		#TODO SAME AS ABOVE NOT NEEDED
+		#new_readset_subset = readset.subset(create_new_readset(readset,sliced_selected_reads))
+
+
+
+
+
+		#final_selected_reads.add(tuple(sliced_selected_reads))
+		#Construction of SNP_read mapping and priority queue
+		(pq,SNP_read_map,phasbale_SNPs,vcf_indices)=__pq_construction_out_of_given_reads( pq,new_readset_subset,positions,SNP_read_map)
+
+		#print('Coverage')
+		#print(coverages.max_coverage_in_range(0,len(vcf_indices)))
+		loop_integer -=1
+
+
+
+
+
+
+	#print('complete read set')
+	#print(final_read_set)
+	#print('Length of the complete Read SEt')
+	#print(len(selected_reads))
 
 	#out= slice_read_selection(pq,coverages,already_covered_SNPs,selected_reads,phasbale_SNPs,max_cov,readset,vcf_indices,SNP_read_map)
-
-
-#TODO insert the bridging .....
-
-	#while True:
-		#Need to find out if pq changes in this method or if by using this method the pq has changed, therefore we
-		#need to construct the pq again (maybe without the already selected reads)
-
-		#slice_selection(pq,coverages,already_covered_SNPs,selected_reads,phasbale_SNPs,max_cov,readset,vcf_indices,SNP_read_map)
-
-
 
 
 	return selected_reads
