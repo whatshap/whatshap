4bc02abbac349dd61676282d602791a31e46880a
Tobias Marschall <tobias.marschall@0ohm.net>
2015-03-23 17:42:20 +0100

    Removed outdated test cases and added a new one for readselection

--- tests/testreadselect.py
+++ tests/testreadselect.py
@@ -1,30 +1,18 @@
+from phasingutils import string_to_readset
+from whatshap.readselect import readselection
 
-from whatshap.readselect import Bestreads
-from ..core import Read, ReadSet, DPTable, IndexSet
+def test_selection():
+	reads = string_to_readset("""
+	  1  1
+	  00
+	  0  1
+	  10  1
+	   1  1
+	    11
+	  0   1
+	""")
+	selected_reads, new_components = readselection(reads, max_cov = 1, bridging = False)
+	assert selected_reads == set([1,5])
+	selected_reads, new_components = readselection(reads, max_cov = 2, bridging = False)
+	assert selected_reads == set([1,3,5]), str(selected_reads)
 
-def test_priority_construction():
-    vcf_indices = {}
-    vcf_indices[100]= '0'
-    vcf_indices[101]='1'
-    vcf_indices[103]='2'
-    print(vcf_indices)
-    #look like in the vcf file
-    read1 = ((100, 'G','Entry(MINOR)'),(101,'T','Entry(MINOR)'))
-    read2 = ((101, 'A','Entry(MINOR)'),(103,'C','Entry(MINOR)'))
-    read3 =((100,'G', 'Entry(MINOR)'),(101, 'A', 'Entry(MINOR)'),(103,'C', 'Entry(MINOR)'))
-    readset= set()
-    readset.add(read1)
-    readset.add(read2)
-    readset.add(read3)
-    print('readset')
-    print(readset)
-
-    br= Bestreads(readset,vcf_indices)
-#    br.priorityqueue_construction(pq)
-
-
-
-def test_read_selection(self, pq,SNP_dict, max_coverage):
-
-
-    Bestreads(readset,vcf_indices)
