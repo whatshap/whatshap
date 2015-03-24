ae35ee0113322850333d9caab01d0e3c73e744e0
Tobias Marschall <tobias.marschall@0ohm.net>
2015-03-24 08:36:47 +0100

    Changed test case: solution was not unique. Also extended it.

--- tests/testreadselect.py
+++ tests/testreadselect.py
@@ -5,14 +5,19 @@ def test_selection():
 	reads = string_to_readset("""
 	  1  1
 	  00
-	  0  1
+	  0   1
 	  10  1
-	   1  1
+	  1   1
 	    11
 	  0   1
+	  1    1
 	""")
 	selected_reads, new_components = readselection(reads, max_cov = 1, bridging = False)
 	assert selected_reads == set([1,5])
 	selected_reads, new_components = readselection(reads, max_cov = 2, bridging = False)
 	assert selected_reads == set([1,3,5]), str(selected_reads)
+	selected_reads, new_components = readselection(reads, max_cov = 3, bridging = False)
+	assert selected_reads == set([1,3,5,7]), str(selected_reads)
+	selected_reads, new_components = readselection(reads, max_cov = 3, bridging = True)
+	assert selected_reads == set([0,1,3,5]), str(selected_reads)
 
