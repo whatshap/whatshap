de8bcd34f93e61ecd82fa580ee25aa7f228f3b6c
Tobias Marschall <tobias.marschall@0ohm.net>
2015-06-15 20:51:33 +0200

    Move C declarations to a pxd files (to allow inclusion by other cython files)

--- whatshap/_core.pyx
+++ whatshap/_core.pyx
@@ -13,29 +13,8 @@ from collections import namedtuple
 Variant = namedtuple('Variant', 'position allele quality')
 
 # ====== Read ======
-cdef extern from "../src/read.h":
-	cdef cppclass Read:
-		Read(string, int, int) except +
-		Read(Read) except +
-		string toString() except +
-		void addVariant(int, int, int) except +
-		string getName() except +
-		vector[int] getMapqs() except +
-		void addMapq(int) except +
-		int getPosition(int) except +
-		void setPosition(int, int)  except +
-		int getAllele(int) except +
-		void setAllele(int, int) except +
-		int getVariantQuality(int) except +
-		void setVariantQuality(int, int) except +
-		int getVariantCount() except +
-		void sortVariants() except +
-		bool isSorted() except +
-		int getSourceID() except +
 
 cdef class PyRead:
-	cdef Read *thisptr
-	cdef bool ownsptr
 
 	def __cinit__(self, str name = None, int mapq = 0, int source_id = 0):
 		cdef string _name = ''
@@ -142,22 +121,8 @@ cdef class PyRead:
 			return self.thisptr.isSorted()
 
 # ====== ReadSet ======
-cdef extern from "../src/readset.h":
-	cdef cppclass ReadSet:
-		ReadSet() except +
-		void add(Read*) except +
-		string toString() except +
-		int size() except +
-		void sort() except +
-		Read* get(int) except +
-		Read* getByName(string, int) except +
-		ReadSet* subset(IndexSet*) except +
-		# TODO: Check why adding "except +" here doesn't compile
-		vector[unsigned int]* get_positions()
-
 
 cdef class PyReadSet:
-	cdef ReadSet *thisptr
 
 	def __cinit__(self):
 		self.thisptr = new ReadSet()
@@ -234,22 +199,9 @@ cdef class PyReadSet:
 		return result
 
 
-# ====== ColumnIterator ======
-cdef extern from "../src/columniterator.h":
-	cdef cppclass ColumnIterator:
-		ColumnIterator(ReadSet) except +
-
 # ====== DPTable ======
-cdef extern from "../src/dptable.h":
-	cdef cppclass DPTable:
-		DPTable(ReadSet*, bool) except +
-		void get_super_reads(ReadSet*) except +
-		int get_optimal_score() except +
-		vector[bool]* get_optimal_partitioning()
-
 
 cdef class PyDPTable:
-	cdef DPTable *thisptr
 
 	def __cinit__(self, PyReadSet readset, all_heterozygous):
 		"""Build the DP table from the given read set which is assumed to be sorted;
@@ -285,17 +237,8 @@ cdef class PyDPTable:
 		return result
 
 # ====== IndexSet ======
-cdef extern from "../src/indexset.h":
-	cdef cppclass IndexSet:
-		IndexSet() except +
-		bool contains(int) except +
-		void add(int) except +
-		int size() except +
-		string toString() except +
-
 
 cdef class PyIndexSet:
-	cdef IndexSet *thisptr
 
 	def __cinit__(self):
 		self.thisptr = new IndexSet()
