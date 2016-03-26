1ae45c493767db33ef988901b74200d4c953cea3
Shilpa Garg <sgarg@mpi-inf.mpg.de>
2016-03-26 12:30:57 +0000

    created triples and triple_genotypes for general pedigree

--- src/pedigree.cpp
+++ src/pedigree.cpp
@@ -21,4 +21,15 @@ void Pedigree::addRelationship(unsigned int m, unsigned int f, unsigned int c) {
 	To get the genotypes, use
 	genotypes_map.at(m), genotypes_map.at(f), and genotypes_map.at(c).
 	*/
+	triple_entry.clear();
+	triple_entry.push_back(m);
+	triple_entry.push_back(f);
+	triple_entry.push_back(c);
+	triples.push_back(triple_entry);
+	
+	//add genotypes now
+	genotype_entry[0]=genotypes_map.at(m);
+	genotype_entry[1]=genotypes_map.at(f);
+	genotype_entry[2]=genotypes_map.at(c);
+	triple_genotypes.push_back(genotype_entry);
 }
