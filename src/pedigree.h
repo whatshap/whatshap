bdf891b4feafb2b69fdf1a7ec2a87a85590e9474
Shilpa Garg <sgarg@mpi-inf.mpg.de>
2016-03-30 19:49:31 +0000

    simplify code

--- src/pedigree.h
+++ src/pedigree.h
@@ -12,8 +12,8 @@
 class Pedigree {
 public:
 	Pedigree();
-	typedef std::vector<unsigned int> triple_entry_t;
-	typedef std::array<std::vector<unsigned int>, 3> genotype_entry_t;
+	typedef std::array<unsigned int, 3> triple_entry_t;
+	//typedef std::array<std::vector<unsigned int>, 3> genotype_entry_t;
 
 	// add genotypes of a single individual
 	void addIndividual(unsigned int individual, std::vector<unsigned int> genotypes);
@@ -25,11 +25,13 @@ public:
 		return individuals.size();
 	}
 	std::vector<triple_entry_t> triples;
-	std::vector<genotype_entry_t> triple_genotypes;
-private:
+	//std::vector<genotype_entry_t> triple_genotypes;
 	std::vector<unsigned int> individuals;
-	// maps individual ids to genotypes
 	std::unordered_map<unsigned int, std::vector<unsigned int>> genotypes_map;
+private:
+	
+	// maps individual ids to genotypes
+
 };
 
 #endif
