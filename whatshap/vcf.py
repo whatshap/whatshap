8f14513009ccf4c41260e63562f6777c6e84fa6e
Marcel Martin <marcel.martin@scilifelab.se>
2016-03-31 18:44:19 +0200

    VcfCall should implement __repr__ instead of __str__

--- whatshap/vcf.py
+++ whatshap/vcf.py
@@ -37,7 +37,7 @@ class VcfCall:
 		self.genotype = genotype
 		assert self.genotype == 2 if self.reference_allele == self.alternative_allele else 1
 
-	def __str__(self):
+	def __repr__(self):
 		return "VcfCall(pos={}, ref={}, alt={}, genotype={})".format(self.position+1,
 			self.reference_allele, self.alternative_allele, self.genotype)
 
