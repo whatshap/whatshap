269d4b98e5811b0fd24cc2865b8e22002aa77869
Tobias Marschall <tobias.marschall@0ohm.net>
2016-04-01 17:17:02 +0200

    Test pedigree phasing

--- tests/phasingutils.py
+++ tests/phasingutils.py
@@ -22,6 +22,20 @@ def string_to_readset(s, w = None):
 	return rs
 
 
+def string_to_readset_trio(s, w = None):
+	s = textwrap.dedent(s).strip()
+	m = { 'M': 1, 'F':2, 'C':0 }
+	read_marks = []
+	s2 = ''
+	for line in s.split('\n'):
+		assert line[0] in m
+		read_marks.append(m[line[0]])
+		s2 += line[1:] + '\n'
+	rs = string_to_readset(s2, w)
+	print('read_marks:', read_marks)
+	return rs, read_marks
+
+
 def matrix_to_readset(lines) :
 
 	rs = ReadSet()
