whatshap/whatshap

commit d7d68b6608920848fb28cdb6efcd9d7c5c4acdfa

Each Read now knows the numeric id of the sample it came from --> we do not need explicitly pass read_sources to PedigreeDPTable any more.

whatshap/core.pyx
@@ -18,8 +18,25 @@ from cython.operator cimport dereference as deref
 # A single variant on a read.
 Variant = namedtuple('Variant', 'position allele quality')
 
+
+cdef class NumericSampleIds:
+	"""
+	Mapping of sample names (strings) to numeric ids.
+	"""
+	def __cinit__(self):
+		self.mapping = {}
+
+	def __getitem__(self, sample):
+		if not sample in self.mapping:
+			self.mapping[sample] = len(self.mapping)
+		return self.mapping[sample]
+
+	def __len__(self):
+		return len(self.mapping)
+
+
 cdef class Read:
-	def __cinit__(self, str name = None, int mapq = 0, int source_id = 0):
+	def __cinit__(self, str name = None, int mapq = 0, int source_id = 0, int sample_id = 0):
 		cdef string _name = ''
 		if name is None:
 			self.thisptr = NULL
@@ -27,7 +44,7 @@ cdef class Read:
 		else:
 			# TODO: Is this the best way to handle string arguments?
 			_name = name.encode('UTF-8')
-			self.thisptr = new cpp.Read(_name, mapq, source_id)
+			self.thisptr = new cpp.Read(_name, mapq, source_id, sample_id)
 			self.ownsptr = True
 
 	def __dealloc__(self):
@@ -241,12 +258,12 @@ cdef class DPTable:
 
 
 cdef class PedigreeDPTable:
-	def __cinit__(self, ReadSet readset, finaldemarcations, recombcost, Pedigree pedigree):
+	def __cinit__(self, ReadSet readset, recombcost, Pedigree pedigree):
 		"""Build the DP table from the given read set which is assumed to be sorted;
 		that is, the variants in each read must be sorted by position and the reads
 		in the read set must also be sorted (by position of their left-most variant).
 		"""
-		self.thisptr = new cpp.PedigreeDPTable(readset.thisptr, finaldemarcations, recombcost, pedigree.thisptr)
+		self.thisptr = new cpp.PedigreeDPTable(readset.thisptr, recombcost, pedigree.thisptr)
 		self.pedigree = pedigree
 
 	def __dealloc__(self):
@@ -291,17 +308,18 @@ cdef class PedigreeDPTable:
 
 
 cdef class Pedigree:
-	def __cinit__(self):
+	def __cinit__(self, numeric_sample_ids):
 		self.thisptr = new cpp.Pedigree()
+		self.numeric_sample_ids = numeric_sample_ids
 
 	def __dealloc__(self):
 		del self.thisptr
 
-	def add_individual(self, int id, vector[unsigned int] genotypes):
-		self.thisptr.addIndividual(id, genotypes)
+	def add_individual(self, id, vector[unsigned int] genotypes):
+		self.thisptr.addIndividual(self.numeric_sample_ids[id], genotypes)
 
-	def add_relationship(self, int mother_id, int father_id, int child_id):
-		self.thisptr.addRelationship(mother_id, father_id, child_id)
+	def add_relationship(self, mother_id, father_id, child_id):
+		self.thisptr.addRelationship(self.numeric_sample_ids[mother_id], self.numeric_sample_ids[father_id], self.numeric_sample_ids[child_id])
 
 	def __len__(self):
 		return self.thisptr.size()

whatshap/cpp.pxd
@@ -9,7 +9,7 @@ from libcpp.vector cimport vector
 
 cdef extern from "../src/read.h":
 	cdef cppclass Read:
-		Read(string, int, int) except +
+		Read(string, int, int, int) except +
 		Read(Read) except +
 		string toString() except +
 		void addVariant(int, int, int) except +
@@ -70,7 +70,7 @@ cdef extern from "../src/pedigree.h":
 
 cdef extern from "../src/pedigreedptable.h":
 	cdef cppclass PedigreeDPTable:
-		PedigreeDPTable(ReadSet*, vector[unsigned int], vector[unsigned int], Pedigree* pedigree) except +
+		PedigreeDPTable(ReadSet*, vector[unsigned int], Pedigree* pedigree) except +
 		void get_super_reads(vector[ReadSet*]*, vector[unsigned int]* transmission_vector) except +
 		int get_optimal_score() except +
 		vector[bool]* get_optimal_partitioning()

whatshap/pedigree.py
@@ -7,6 +7,7 @@ from collections import namedtuple, Counter, OrderedDict
 
 RecombinationMapEntry = namedtuple('RecombinationMapEntry', ['position', 'cum_distance'])
 
+
 def interpolate(point, start_pos, end_pos, start_value, end_value):
 	assert start_pos <= point <= end_pos
 	if start_pos == point == end_pos:
@@ -171,12 +172,15 @@ class PedReader:
 	All fields except the individual, maternal and paternal ID are ignored by
 	this class. The entire file is read upon construction.
 	"""
-	def __init__(self, file):
+	def __init__(self, file, numeric_sample_ids):
 		if isinstance(file, str):
 			with open(file) as f:
 				self.individuals = self._parse(f)
 		else:
 			self.individuals = self._parse(file)
+		# Ensure that all individuals mentioned in the ped have a numeric id
+		for individual in self.individuals:
+			numeric_sample_ids[individual.id]
 
 	@staticmethod
 	def _parse_record(line):

src/dptable.cpp
@@ -298,8 +298,8 @@ void DPTable::get_super_reads(ReadSet* output_read_set) {
   ColumnIterator column_iterator(*read_set);
   const vector<unsigned int>* positions = column_iterator.get_positions();
 
-  Read* r0 = new Read("superread0", -1, 0);
-  Read* r1 = new Read("superread1", -1, 0);
+  Read* r0 = new Read("superread0", -1, 0, -1);
+  Read* r1 = new Read("superread1", -1, 0, -1);
   
   if (backtrace_table.empty()) {
     assert(!column_iterator.has_next());

src/pedigreedptable.cpp
@@ -14,13 +14,10 @@
 
 using namespace std;
 
-// TODO read_marks is unnecessary because every read in the read_set already
-// knows its id
-PedigreeDPTable::PedigreeDPTable(ReadSet* read_set, vector<unsigned int> read_marks, vector<unsigned int> recombcost, const Pedigree* pedigree)
+PedigreeDPTable::PedigreeDPTable(ReadSet* read_set, const vector<unsigned int>& recombcost, const Pedigree* pedigree)
 	:
 	read_set(read_set),
-	read_marks(std::move(read_marks)),
-	recombcost(std::move(recombcost)),
+	recombcost(recombcost),
 	pedigree(pedigree),
 	optimal_score(0u),
 	optimal_score_index(0u),
@@ -32,6 +29,11 @@ PedigreeDPTable::PedigreeDPTable(ReadSet* read_set, vector<unsigned int> read_ma
   
   for (size_t i=0; i<std::pow(4, pedigree->triple_count()); ++i) {
     pedigree_partitions.push_back(new PedigreePartitions(*pedigree, i));
+  }
+  
+  // translate all individual ids to individual indices
+  for (size_t i=0; i<read_set->size(); ++i) {
+    read_marks.push_back(pedigree->id_to_index(read_set->get(i)->getSampleID()));
   }
   
   compute_table();
@@ -399,7 +401,7 @@ void PedigreeDPTable::get_super_reads(std::vector<ReadSet*>* output_read_set, ve
   std::vector<std::pair<Read*,Read*>> superreads;
   for(unsigned int i=0;i<pedigree->size();i++)
   {
-     superreads.emplace_back(new Read("superread_0_"+std::to_string(i), -1, 0),new Read("superread_1_"+std::to_string(i), -1, 0)) ;     
+     superreads.emplace_back(new Read("superread_0_"+std::to_string(i), -1, 0, -1),new Read("superread_1_"+std::to_string(i), -1, 0, -1));
   }
   
   if (index_backtrace_table.empty()) {

src/pedigreedptable.h
@@ -24,8 +24,8 @@ class PedigreeDPTable {
 private:
   ReadSet* read_set;
   
-  const std::vector<unsigned int> read_marks;
-  const std::vector<unsigned int> recombcost;
+  std::vector<unsigned int> read_marks;
+  const std::vector<unsigned int>& recombcost;
   const Pedigree* pedigree;
   std::vector<PedigreePartitions*> pedigree_partitions;
   // vector of indexingschemes
@@ -59,7 +59,7 @@ public:
    *  @param all_heterozygous If true, then the "all heterozygous" assumption is made;
    *                          i.e., all positions are forced to be heterozygous even when
    *                          reads suggest a homozygous site. */
-  PedigreeDPTable(ReadSet* read_set, std::vector<unsigned int> read_marks, std::vector<unsigned int> recombcost, const Pedigree* pedigree);
+  PedigreeDPTable(ReadSet* read_set, const std::vector<unsigned int>& recombcost, const Pedigree* pedigree);
  
   ~PedigreeDPTable();
 

src/read.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-Read::Read(const std::string& name, int mapq, int source_id) : name(name), mapqs(1, mapq), source_id(source_id) {
+Read::Read(const std::string& name, int mapq, int source_id, int sample_id) : name(name), mapqs(1, mapq), source_id(source_id), sample_id(sample_id) {
 	this->id = -1;
 }
 
@@ -122,6 +122,10 @@ void Read::addMapq(int mapq) {
 
 int Read::getSourceID() const {
 	return source_id;
+}
+
+int Read::getSampleID() const {
+	return sample_id;
 }
 
 bool Read::isSorted() const {

src/read.h
@@ -9,7 +9,7 @@
 
 class Read {
 public:
-	Read(const std::string& name, int mapq, int source_id);
+	Read(const std::string& name, int mapq, int source_id, int sample_id);
 	virtual ~Read() {}
 	std::string toString();
 	void addVariant(int position, int allele, int quality);
@@ -34,6 +34,7 @@ public:
 	const std::vector<int>& getMapqs() const;
 	void addMapq(int mapq);
 	int getSourceID() const;
+	int getSampleID() const;
 	bool isSorted() const;
 private:
 	typedef struct enriched_entry_t {
@@ -53,6 +54,7 @@ private:
 	std::string name;
 	std::vector<int> mapqs;
 	int source_id;
+	int sample_id;
 	int id;
 	std::vector<enriched_entry_t> variants;
 };

commit b6410ce1ccce97ebcfaa5d6b15bcc0d14b8e7714

Fix handling of paired end reads (broken in commit d7d68b6 when numeric sample IDs were introduced).

whatshap/core.pyx
@@ -54,8 +54,8 @@ cdef class Read:
 
 	def __repr__(self):
 		assert self.thisptr != NULL
-		return 'Read(name={!r}, mapq={}, source_id={}, variants={})'.format(
-			self.name, self.mapqs, self.source_id, list(self))
+		return 'Read(name={!r}, mapq={}, source_id={}, sample_id={}, variants={})'.format(
+			self.name, self.mapqs, self.source_id, self.sample_id, list(self))
 
 	property mapqs:
 		def __get__(self):
@@ -71,6 +71,11 @@ cdef class Read:
 		def __get__(self):
 			assert self.thisptr != NULL
 			return self.thisptr.getSourceID()
+
+	property sample_id:
+		def __get__(self):
+			assert self.thisptr != NULL
+			return self.thisptr.getSampleID()
 
 	def __iter__(self):
 		"""Iterate over all variants in this read"""

whatshap/cpp.pxd
@@ -26,6 +26,7 @@ cdef extern from "../src/read.h":
 		void sortVariants() except +
 		bool isSorted() except +
 		int getSourceID() except +
+		int getSampleID() except +
 
 
 cdef extern from "../src/indexset.h":

whatshap/pedigree.py
@@ -4,7 +4,6 @@ Pedigree-related functions
 import math
 from collections import namedtuple, Counter, OrderedDict
 
-
 RecombinationMapEntry = namedtuple('RecombinationMapEntry', ['position', 'cum_distance'])
 
 

whatshap/variants.py
@@ -126,7 +126,7 @@ class ReadSetReader:
 			for position, allele, quality in detected:
 				read.add_variant(position, allele, quality)
 			if read:  # At least one variant covered and detected
-				reads[(alignment.source_id, alignment.bam_alignment.qname)].append(read)
+				reads[(alignment.source_id, alignment.bam_alignment.qname, self._numeric_sample_ids[sample])].append(read)
 		return reads
 
 	@staticmethod
@@ -438,7 +438,7 @@ class ReadSetReader:
 		modified.
 		"""
 		if read2:
-			result = Read(read1.name, read1.mapqs[0], read1.source_id, read1.numeric_sample_id)
+			result = Read(read1.name, read1.mapqs[0], read1.source_id, read1.sample_id)
 			result.add_mapq(read2.mapqs[0])
 		else:
 			return read1

tests/data/paired_end.sorted.sam
@@ -1,0 +1,95 @@
+@HD	VN:1.4	GO:none	SO:coordinate
+@SQ	SN:1	LN:249250621
+@SQ	SN:2	LN:243199373
+@SQ	SN:3	LN:198022430
+@SQ	SN:4	LN:191154276
+@SQ	SN:5	LN:180915260
+@SQ	SN:6	LN:171115067
+@SQ	SN:7	LN:159138663
+@SQ	SN:8	LN:146364022
+@SQ	SN:9	LN:141213431
+@SQ	SN:10	LN:135534747
+@SQ	SN:11	LN:135006516
+@SQ	SN:12	LN:133851895
+@SQ	SN:13	LN:115169878
+@SQ	SN:14	LN:107349540
+@SQ	SN:15	LN:102531392
+@SQ	SN:16	LN:90354753
+@SQ	SN:17	LN:81195210
+@SQ	SN:18	LN:78077248
+@SQ	SN:19	LN:59128983
+@SQ	SN:20	LN:63025520
+@SQ	SN:21	LN:48129895
+@SQ	SN:22	LN:51304566
+@SQ	SN:X	LN:155270560
+@SQ	SN:Y	LN:59373566
+@SQ	SN:MT	LN:16569
+@SQ	SN:GL000207.1	LN:4262
+@SQ	SN:GL000226.1	LN:15008
+@SQ	SN:GL000229.1	LN:19913
+@SQ	SN:GL000231.1	LN:27386
+@SQ	SN:GL000210.1	LN:27682
+@SQ	SN:GL000239.1	LN:33824
+@SQ	SN:GL000235.1	LN:34474
+@SQ	SN:GL000201.1	LN:36148
+@SQ	SN:GL000247.1	LN:36422
+@SQ	SN:GL000245.1	LN:36651
+@SQ	SN:GL000197.1	LN:37175
+@SQ	SN:GL000203.1	LN:37498
+@SQ	SN:GL000246.1	LN:38154
+@SQ	SN:GL000249.1	LN:38502
+@SQ	SN:GL000196.1	LN:38914
+@SQ	SN:GL000248.1	LN:39786
+@SQ	SN:GL000244.1	LN:39929
+@SQ	SN:GL000238.1	LN:39939
+@SQ	SN:GL000202.1	LN:40103
+@SQ	SN:GL000234.1	LN:40531
+@SQ	SN:GL000232.1	LN:40652
+@SQ	SN:GL000206.1	LN:41001
+@SQ	SN:GL000240.1	LN:41933
+@SQ	SN:GL000236.1	LN:41934
+@SQ	SN:GL000241.1	LN:42152
+@SQ	SN:GL000243.1	LN:43341
+@SQ	SN:GL000242.1	LN:43523
+@SQ	SN:GL000230.1	LN:43691
+@SQ	SN:GL000237.1	LN:45867
+@SQ	SN:GL000233.1	LN:45941
+@SQ	SN:GL000204.1	LN:81310
+@SQ	SN:GL000198.1	LN:90085
+@SQ	SN:GL000208.1	LN:92689
+@SQ	SN:GL000191.1	LN:106433
+@SQ	SN:GL000227.1	LN:128374
+@SQ	SN:GL000228.1	LN:129120
+@SQ	SN:GL000214.1	LN:137718
+@SQ	SN:GL000221.1	LN:155397
+@SQ	SN:GL000209.1	LN:159169
+@SQ	SN:GL000218.1	LN:161147
+@SQ	SN:GL000220.1	LN:161802
+@SQ	SN:GL000213.1	LN:164239
+@SQ	SN:GL000211.1	LN:166566
+@SQ	SN:GL000199.1	LN:169874
+@SQ	SN:GL000217.1	LN:172149
+@SQ	SN:GL000216.1	LN:172294
+@SQ	SN:GL000215.1	LN:172545
+@SQ	SN:GL000205.1	LN:174588
+@SQ	SN:GL000219.1	LN:179198
+@SQ	SN:GL000224.1	LN:179693
+@SQ	SN:GL000223.1	LN:180455
+@SQ	SN:GL000195.1	LN:182896
+@SQ	SN:GL000212.1	LN:186858
+@SQ	SN:GL000222.1	LN:186861
+@SQ	SN:GL000200.1	LN:187035
+@SQ	SN:GL000193.1	LN:189789
+@SQ	SN:GL000194.1	LN:191469
+@SQ	SN:GL000225.1	LN:211173
+@SQ	SN:GL000192.1	LN:547496
+@RG	ID:mother	PU:UNKNOWN	LB:UNKNOWN	SM:mother	PL:PACBIO
+@RG	ID:father	PU:UNKNOWN	LB:UNKNOWN	SM:father	PL:PACBIO
+@PG	ID:GATK TableRecalibration	VN:1.0.5069	CL:default_read_group=null default_platform=null force_read_group=null force_platform=null window_size_nqs=5 homopolymer_nback=7 exception_if_no_tile=false solid_recal_mode=SET_Q_ZERO solid_nocall_strategy=THROW_EXCEPTION recal_file=/target/gpfs2/gcc/from_millipede/fourth_batch/mother/mother.FC81999ABXX_L6.al10.covariates_before.ftl.human_g1k_v37.matefixed.covariate_table.csv output_bam=null preserve_qscores_less_than=5 smoothing=1 max_quality_score=50 no_pg_tag=false fail_with_no_eof_marker=false skipUQUpdate=false Covariates=[ReadGroupCovariate, QualityScoreCovariate, CycleCovariate, DinucCovariate] 
+@PG	ID:GATK TableRecalibration.1	VN:1.0.5069	CL:default_read_group=null default_platform=null force_read_group=null force_platform=null window_size_nqs=5 homopolymer_nback=7 exception_if_no_tile=false solid_recal_mode=SET_Q_ZERO solid_nocall_strategy=THROW_EXCEPTION recal_file=/target/gpfs2/gcc/from_millipede/fourth_batch/mother/mother.FC81986ABXX_L6.al10.covariates_before.ftl.human_g1k_v37.matefixed.covariate_table.csv output_bam=null preserve_qscores_less_than=5 smoothing=1 max_quality_score=50 no_pg_tag=false fail_with_no_eof_marker=false skipUQUpdate=false Covariates=[ReadGroupCovariate, QualityScoreCovariate, CycleCovariate, DinucCovariate] 
+@PG	ID:GATK TableRecalibration.1.1	VN:1.0.5069	CL:default_read_group=null default_platform=null force_read_group=null force_platform=null window_size_nqs=5 homopolymer_nback=7 exception_if_no_tile=false solid_recal_mode=SET_Q_ZERO solid_nocall_strategy=THROW_EXCEPTION recal_file=/target/gpfs2/gcc/from_millipede/fourth_batch/father/father.FC81986ABXX_L7.al10.covariates_before.ftl.human_g1k_v37.matefixed.covariate_table.csv output_bam=null preserve_qscores_less_than=5 smoothing=1 max_quality_score=50 no_pg_tag=false fail_with_no_eof_marker=false skipUQUpdate=false Covariates=[ReadGroupCovariate, QualityScoreCovariate, CycleCovariate, DinucCovariate] 
+@PG	ID:GATK TableRecalibration.3	VN:1.0.5069	CL:default_read_group=null default_platform=null force_read_group=null force_platform=null window_size_nqs=5 homopolymer_nback=7 exception_if_no_tile=false solid_recal_mode=SET_Q_ZERO solid_nocall_strategy=THROW_EXCEPTION recal_file=/target/gpfs2/gcc/from_millipede/fourth_batch/father/father.FC81999ABXX_L7.al10.covariates_before.ftl.human_g1k_v37.matefixed.covariate_table.csv output_bam=null preserve_qscores_less_than=5 smoothing=1 max_quality_score=50 no_pg_tag=false fail_with_no_eof_marker=false skipUQUpdate=false Covariates=[ReadGroupCovariate, QualityScoreCovariate, CycleCovariate, DinucCovariate] 
+A81999633#0	99	1	79978	0	90M	=	80355	466	ATGGAACCAAAAAAGAGCCCACATAGCCAAAGCAAGACTAAGCAAAAAGAATAAATCTAGAGGCATCACATTACTCGACTTCAAACTATA	CCCEEFBDDEEEEEDEDCCCDADDDCBCDEECCDDCDADDDCBDDDDDCDDCDDDCBDDCDCCBDCCDADBEDAEC;EBEECEFEBFEDC	X0:i:3	X1:i:1	XA:Z:6,+69982,90M,0;1,+79978,90M,0;15,-102452268,90M,1;	MD:Z:90	XG:i:0	AM:i:0	NM:i:0	SM:i:0	XM:i:0	XO:i:0	MQ:i:0	OQ:Z:ggggeggggggggfggggggggggfggggggeeggggggggggdgdgggggggegggggggegggeggde`ceddfggggggggegggge	XT:A:R	RG:Z:father
+A8199963#0	163	1	80045	36	90M	=	80435	479	ACATTACTCGACTTCAAACTATACTATAAGGCCATAGTCACCAAAACAGCATGGTACTGGTATAAAAATAGGCATATAGACCAATGGAAT	?@CCDCBDC:EBDDCDDDADCBCADCBCDADCDBBCCBBDBCDEEDADCDCCCCBC@DCACCDCEDDDACCCBCCCCEAEBAEDDCCAFC	X0:i:4	X1:i:0	XA:Z:15,-102452201,90M,0;19,+121642,90M,0;6,+70049,90M,0;	MD:Z:90	XG:i:0	AM:i:0	NM:i:0	SM:i:0	XM:i:0	XO:i:0	MQ:i:37	OQ:Z:fdddddd_ccffeedeecddeaecdcadd_fcf__edeeffefffeffdfcddd`cbbcbecfcfddd_dceeededf_ff_gcecb]fe	XT:A:R	RG:Z:mother
+A81999633#0	147	1	80355	0	90M	=	79978	-466	TAAAAATTCTAGAAGATAAGATTGGAAAAACCCTTCTAGACATTGGCTTAGGCAAAGACTTCACAATCAAGAACCCAAAAGCAAACACAA	DFEGFDDDDDEDEECCDEDCDEDCCEEDECCCCDECCDBCBCDCCBCDDDCBDEEDBCCEDCCCECDCEDCEBCDCEEEECCFFDCACE@	X0:i:3	X1:i:1	XA:Z:1,-80355,90M,0;19,-121952,90M,0;15,+102451891,90M,1;	MD:Z:90	XG:i:0	AM:i:0	NM:i:0	SM:i:0	XM:i:0	XO:i:0	MQ:i:0	OQ:Z:egcfgedafdgfgggeggggggdggggdgdggggfggggggggeggggggggfgggggggggegggggggggegggggggggggggcggg	XT:A:R	RG:Z:father
+A8199963#0	83	1	80435	37	90M	=	80045	-479	GCAAACACAACAAAACAAAGATAAATAGATGGGACTTAATTAAACTGAAAGCCTTCTGCACATCAAAATAAATAATCAGCAGAGTAAACA	AAEFDCDDECCECDCCEDDCCBDECBDACDCCACCDCDCDCDDCCCCDDEBCCDDCDBCBCCDCEEEC>EEC@EDDCEC>E?EBDDDA@A	X0:i:1	X1:i:0	MD:Z:90	XG:i:0	AM:i:0	NM:i:0	SM:i:37	XM:i:0	XO:i:0	MQ:i:36	OQ:Z:aaeeeeedeeddddededecd^eceae^eaee^eeeebceebedeaebdceeddeecaeeeeedgeee^eee`eeeeeededeeedec_e	XT:A:U	RG:Z:mother

tests/data/paired_end.sorted.vcf
@@ -1,0 +1,5 @@
+##fileformat=VCFv4.1
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	mother	father	child
+1	80050	.	G	A	.	.	.	GT	0/1	0/0	0/0
+1	80060	.	G	A	.	.	.	GT	0/1	0/0	0/0
+1	80438	.	G	A	.	.	.	GT	0/1	0/0	0/1

tests/data/trio_paired_end.ped
@@ -1,0 +1,3 @@
+trio mother 0 0 0 0
+trio father 0 0 0 0
+trio child father mother 0 0

tests/tests.py
@@ -9,6 +9,7 @@ from whatshap.vcf import VcfReader, VariantCallPhase
 
 trio_bamfile = 'tests/data/trio.pacbio.bam'
 trio_merged_bamfile = 'tests/data/trio-merged-blocks.bam'
+trio_paired_end_bamfile = 'tests/data/paired_end.sorted.bam'
 
 
 def setup_module():
@@ -17,6 +18,8 @@ def setup_module():
 	pysam.index(trio_bamfile, catch_stdout=False)
 	pysam.view('tests/data/trio-merged-blocks.sam', '-b', '-o', trio_merged_bamfile, catch_stdout=False)
 	pysam.index(trio_merged_bamfile, catch_stdout=False)
+	pysam.view('tests/data/paired_end.sorted.sam', '-b', '-o', trio_paired_end_bamfile, catch_stdout=False)
+	pysam.index(trio_paired_end_bamfile, catch_stdout=False)
 
 
 def teardown_module():
@@ -24,6 +27,8 @@ def teardown_module():
 	os.remove(trio_bamfile + '.bai')
 	os.remove(trio_merged_bamfile)
 	os.remove(trio_merged_bamfile + '.bai')
+	os.remove(trio_paired_end_bamfile)
+	os.remove(trio_paired_end_bamfile + '.bai')
 
 
 def test_pysam_version():
@@ -245,3 +250,28 @@ def test_phase_specific_chromosome():
 					assert_phasing(table.phases_of('HG004'), [None, None, None, None, None])
 					assert_phasing(table.phases_of('HG003'), [None, None, None, None, None])
 					assert_phasing(table.phases_of('HG002'), [None, None, None, None, None])
+
+
+def test_phase_trio_paired_end_reads():
+	with TemporaryDirectory() as tempdir:
+		outvcf = tempdir + '/output-paired_end.vcf'
+		run_whatshap(phase_input_files=[trio_paired_end_bamfile], variant_file='tests/data/paired_end.sorted.vcf', output=outvcf,
+		        ped='tests/data/trio_paired_end.ped', genmap='tests/data/trio.map')
+		assert os.path.isfile(outvcf)
+
+		tables = list(VcfReader(outvcf))
+		assert len(tables) == 1
+		table = tables[0]
+		assert table.chromosome == '1'
+		assert len(table.variants) == 3
+		assert table.samples == ['mother', 'father', 'child']
+		assert table.num_of_blocks_of('mother') == 1
+		assert table.num_of_blocks_of('father') == 0
+		assert table.num_of_blocks_of('child') == 1
+
+		phase0 = VariantCallPhase(80050, 0, None)
+		phase1 = VariantCallPhase(80050, 1, None)
+
+		assert_phasing(table.phases_of('mother'), [phase1, phase1, phase0])
+		assert_phasing(table.phases_of('father'), [None, None, None])
+		assert_phasing(table.phases_of('child'), [None, None, phase1])