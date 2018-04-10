d75c5b3357bf6643f6ec292bfcdc42b28d626ca7
Marcel Martin <marcel.martin@scilifelab.se>
2018-04-10 17:12:56 +0200

    Add doctests

--- whatshap/compare.py
+++ whatshap/compare.py
@@ -75,11 +75,19 @@ class PhasingErrors:
 
 
 def complement(s):
+	"""
+	>>> complement('01100')
+	'10011'
+	"""
 	t = {'0': '1', '1':'0'}
 	return ''.join(t[c] for c in s)
 
 
 def hamming(s0, s1):
+	"""
+	>>> hamming('ABCD', 'AXCY')
+	2
+	"""
 	assert len(s0) == len(s1)
 	return sum(c0 != c1 for c0, c1 in zip(s0, s1))
 
