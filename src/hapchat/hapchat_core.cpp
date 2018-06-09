31316be7cea422d9b2091ad8fa2e20bde34594ab
Murray Patterson <murray.patterson@gmail.com>
2018-06-09 14:34:15 +0200

    squash GPL mention that made it back in somehow

--- src/hapchat/hapchat_core.cpp
+++ src/hapchat/hapchat_core.cpp
@@ -1,29 +1,3 @@
-/**
- *
- * HapCHAT: Adaptive haplotype assembly for efficiently leveraging
- * high coverage in long read HapCHAT
- *
- * Copyright (C) 2017  Stefano Beretta, Murray Patterson and Simone Zaccaria
- *
- * Distributed under the terms of the GNU General Public License (GPL)
- *
- * This file is part of HapCHAT.
- *
- * HapCHAT is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * HapCHAT is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with HapCHAT.  If not, see <http://www.gnu.org/licenses/>.
- *
- **/
-
 #include <stdlib.h>
 #include <string.h>
 #include <limits>
