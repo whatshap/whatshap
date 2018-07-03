dc69b2f8f27a412b471c6f119a076a65022d8229
Murray Patterson <murray.patterson@gmail.com>
2018-07-04 01:35:00 +0200

    make merge_reads a separate procedure, in fact, make it that of merge.py, no need to keep it separate

--- whatshap/phase.py
+++ whatshap/phase.py
@@ -9,16 +9,18 @@ import logging
 import sys
 import platform
 import resource
+
 from collections import defaultdict
 from copy import deepcopy
+from math import log
 
 from xopen import xopen
+from networkx import Graph, number_of_nodes, number_of_edges, connected_components, node_connected_component, shortest_path
 
 from contextlib import ExitStack
 from .vcf import VcfReader, PhasedVcfWriter, GenotypeLikelihoods
 from . import __version__
-from .core import ReadSet, readselection, Pedigree, PedigreeDPTable, NumericSampleIds, PhredGenotypeLikelihoods, compute_genotypes
-from .merge import merge_reads
+from .core import Read, ReadSet, readselection, Pedigree, PedigreeDPTable, NumericSampleIds, PhredGenotypeLikelihoods, compute_genotypes
 from .graph import ComponentFinder
 from .pedigree import (PedReader, mendelian_conflict, recombination_cost_map,
                        load_genetic_map, uniform_recombination_map, find_recombination)
@@ -155,26 +157,224 @@ def read_reads(readset_reader, chromosome, variants, sample, fasta, phase_input_
 	return readset, vcf_source_ids
 
 
-def select_reads(
-		readset,
-		max_coverage,
-		preferred_source_ids,
-		merge_reads_ = False,
-		merge_reads_error_rate = 0.15,
-		merge_reads_max_error_rate = 0.25,
-		merge_reads_positive_threshold = 100000,
-		merge_reads_negative_threshold = 1000
-	):
+def eval_overlap(n1, n2):
+	"""
+	Return a tuple containing the number of matches (resp.,
+	mismatches) between a pair (n1,n2) of overlapping reads
+	"""
+	hang1 = n2['begin'] - n1['begin']
+	overlap = zip(n1['sites'][hang1:], n2['sites'])
+	match, mismatch = (0, 0)
+	for (c1, c2) in overlap:
+		if c1 in ['A', 'C', 'G', 'T'] and c1 in ['A', 'C', 'G', 'T']:
+			if c1 ==  c2:
+				match += 1
+			else:
+				mismatch += 1
+	return (match, mismatch)
 
-	readset = readset.subset([i for i, read in enumerate(readset) if len(read) >= 2])
-	logger.info('Kept %d reads that cover at least two variants each', len(readset))
 
-	if merge_reads_ :
-		logger.info('Merging %d reads with error rate %.2f, maximum error rate %.2f, positive threshold %d and negative threshold %d ...', len(readset), merge_reads_error_rate, merge_reads_max_error_rate, merge_reads_positive_threshold, merge_reads_negative_threshold)
-		previous_length = len(readset)
-		readset = merge_reads(readset, merge_reads_error_rate, merge_reads_max_error_rate, merge_reads_positive_threshold, merge_reads_negative_threshold)
-		logger.info('... after merging: merged %d reads into %d reads', previous_length, len(readset))
+def merge_reads(readset, error_rate, max_error_rate, threshold, neg_threshold) :
+	"""
+	Return a set of reads after merging together subsets of reads
+	(into super reads) from an input readset according to a
+	probabilistic model of how likely sets of reads are to appear
+	together on one haplotype and on opposite haplotypes.
+	readset -- the input .core.ReadSet object
+	error_rate -- the probability that a nucleotide is wrong
+	max_error_rate -- the maximum error rate of any edge of the read
+	merging graph allowed before we discard it
+	threshold -- the threshold of the ratio between the probabilities
+	that a pair ' 'of reads come from the same haplotype and different
+	haplotypes
+	neg_threshold -- The threshold of the ratio between the
+	probabilities that a pair of reads come from the same haplotype
+	and different haplotypes.
+	"""
+	logger.info('Merging %d reads with error rate %.2f, maximum error rate %.2f, positive threshold %d and negative threshold %d ...', len(readset), error_rate, max_error_rate, threshold, neg_threshold)
+	logger.debug("Merging started.")
+	gblue = Graph()
+	gred = Graph()
+	gnotblue = Graph()
+	gnotred = Graph()
 
+	# Probability that any nucleotide is wrong
+	logger.debug("Error Rate: %s", error_rate)
+
+	# If an edge has too many errors, we discard it, since it is not reliable
+	logger.debug("Max Error Rate: %s", max_error_rate)
+
+	# Threshold of the ratio between the probabilities that the two reads come from
+	# the same side or from different sides
+	thr = threshold
+	logger.debug("Positive Threshold: %s", thr)
+
+	# Threshold_neg is a more conservative threshold for the evidence
+	# that two reads should not be clustered together.
+	thr_neg = neg_threshold
+	logger.debug("Negative Threshold: %s", thr_neg)
+
+	thr_diff = 1 + int(log(thr, (1 - error_rate) / (error_rate / 3)))
+	thr_neg_diff = 1 + int(log(thr_neg, (1 - error_rate) / (error_rate / 3)))
+	logger.debug("Thr. Diff.: %s - Thr. Neg. Diff.: %s",
+		thr_diff,
+		thr_neg_diff)
+
+	logger.debug("Start reading the reads...")
+	id = 0
+	orig_reads = {}
+	site_alleles = {}
+	queue = {}
+	reads = {}
+	for read in readset:
+		id += 1
+		begin_str =read[0][0]
+		snps = []
+		orgn=[]
+		for variant in read:
+
+			site = variant[0]
+			zyg = variant[1]
+			qual = variant[2]
+
+			orgn.append([str(site),str(zyg),str(qual)])
+			if int(zyg) == 0:
+				snps.append('G')
+			else:
+				snps.append('C')
+
+		begin = int(begin_str)
+		end = begin + len(snps)
+		orig_reads[id]=orgn
+		logger.debug("id: %s - pos: %s - snps: %s", id, begin, "".join(snps))
+
+
+		gblue.add_node(id, begin=begin, end=end, sites="".join(snps))
+		gnotblue.add_node(id, begin=begin, end=end, sites="".join(snps))
+		gred.add_node(id, begin=begin, end=end, sites="".join(snps))
+		gnotred.add_node(id, begin=begin, end=end, sites="".join(snps))
+		queue[id] = {'begin': begin, 'end': end, 'sites': snps}
+		reads[id] = {'begin': begin, 'end': end, 'sites': snps}
+		for x in [id for id in queue.keys() if queue[id]['end'] <= begin]:
+			del queue[x]
+		for id1 in queue.keys():
+			if id == id1:
+				continue
+			match, mismatch = eval_overlap(queue[id1], queue[id])
+			if match + mismatch >= thr_neg_diff and min(match, mismatch) / (match + mismatch) <= max_error_rate and match - mismatch >= thr_diff:
+				gblue.add_edge(id1, id, match=match, mismatch=mismatch)
+				if mismatch - match >= thr_diff:
+					gred.add_edge(id1, id, match=match, mismatch=mismatch)
+				if match - mismatch >= thr_neg_diff:
+					gnotred.add_edge(id1, id, match=match, mismatch=mismatch)
+				if mismatch - match >= thr_neg_diff:
+					gnotblue.add_edge(id1, id, match=match, mismatch=mismatch)
+
+	logger.debug("Finished reading the reads.")
+	logger.debug("Number of reads: %s", id)
+	logger.debug("Blue Graph")
+	logger.debug("Nodes: %s - Edges: %s - ConnComp: %s",
+		number_of_nodes(gblue),
+		number_of_edges(gblue),
+		len(list(connected_components(gblue))))
+	logger.debug("Non-Blue Graph")
+	logger.debug("Nodes: %s - Edges: %s - ConnComp: %s",
+		number_of_nodes(gnotblue),
+		number_of_edges(gnotblue),
+		len(list(connected_components(gnotblue))))
+	logger.debug("Red Graph")
+	logger.debug("Nodes: %s - Edges: %s - ConnComp: %s",
+		number_of_nodes(gred),
+		number_of_edges(gred),
+		len(list(connected_components(gred))))
+	logger.debug("Non-Red Graph")
+	logger.debug("Nodes: %s - Edges: %s - ConnComp: %s",
+		number_of_nodes(gnotred),
+		number_of_edges(gnotred),
+		len(list(connected_components(gnotred))))
+
+	# We consider the notblue edges as an evidence that two reads
+	# should not be merged together
+	# Since we want to merge each blue connected components into
+	# a single superread, we check each notblue edge (r1, r2) and
+	# we remove some blue edges so that r1 and r2 are not in the
+	# same blue connected component
+
+	blue_component = {}
+	current_component = 0;
+	for conncomp in connected_components(gblue):
+		for v in conncomp:
+			blue_component[v] = current_component
+		current_component += 1
+
+	# Keep only the notblue edges that are inside a blue connected component
+	good_notblue_edges = [(v, w) for (v, w) in gnotblue.edges() if blue_component[v] == blue_component[w]]
+
+	notblue_counter = 0
+	num_notblue = len(good_notblue_edges)
+	block = num_notblue // 100
+	num_blue = len(list(connected_components(gblue)))
+	for (u, v) in good_notblue_edges:
+		while v in node_connected_component(gblue, u):
+			path = shortest_path(gblue, source=u, target=v)
+			# Remove the edge with the smallest support
+			# A better strategy is to weight each edge with -log p
+			# and remove the minimum (u,v)-cut
+			w, x = (min(zip(path[:-1], path[1:]),
+				key=lambda p: gblue[p[0]][p[1]]['match'] - gblue[p[0]][p[1]]['mismatch']))
+			gblue.remove_edge(w, x)
+
+	# Merge blue components (somehow)
+	logger.debug("Started Merging Reads...")
+	superreads = {} # superreads given by the clusters (if clustering)
+	rep = {} # cluster representative of a read in a cluster
+
+	for cc in connected_components(gblue):
+		if len(cc) > 1 :
+			r = min(cc)
+			superreads[r] = {}
+			for id in cc:
+				rep[id] = r
+			logger.debug("rep: %s - cc: %s", r, ",".join([str(id) for id in cc]))
+
+	for id in orig_reads:
+		if id in rep:
+			for tok in orig_reads[id]:
+				site = int(tok[0])
+				zyg = int(tok[1])
+				qual = int(tok[2])
+				r = rep[id]
+				if site not in superreads[r]:
+					superreads[r][site] = [0,0]
+				superreads[r][site][zyg] += qual
+
+		merged_reads=ReadSet()
+		readn=0
+		for id in orig_reads:
+			read=Read("read"+str(readn))
+			readn+=1
+			if id in rep:
+				if id == rep[id]:
+					for site in sorted(superreads[id]):
+						z = superreads[id][site]
+						if z[0] >= z[1]:
+							read.add_variant(site,0,z[0]-z[1])
+
+						elif z[1] > z[0]:
+							read.add_variant(site,1,z[1]-z[0])
+					merged_reads.add(read)
+			else:
+				for tok in orig_reads[id]:
+					read.add_variant(int(tok[0]),int(tok[1]),int(tok[2]))
+				merged_reads.add(read)
+
+	logger.debug("Finished merging reads.")
+	logger.info('... after merging: merged %d reads into %d reads', len(readset), len(merged_reads))
+
+	return merged_reads
+
+
+def select_reads(readset, max_coverage, preferred_source_ids):
 	logger.info('Reducing coverage to at most %dX by selecting most informative reads ...', max_coverage)
 	selected_indices = readselection(readset, max_coverage, preferred_source_ids)
 	selected_reads = readset.subset(selected_indices)
@@ -567,7 +767,12 @@ def run_whatshap(
 					# TODO: Read selection done w.r.t. all variants, where using heterozygous variants only
 					# TODO: would probably give better results.
 					with timers('select'):
-						selected_reads = select_reads(readset, max_coverage_per_sample, preferred_source_ids = vcf_source_ids, merge_reads_ = merge_reads_, merge_reads_error_rate = merge_reads_error_rate, merge_reads_max_error_rate = merge_reads_max_error_rate, merge_reads_positive_threshold = merge_reads_positive_threshold, merge_reads_negative_threshold = merge_reads_negative_threshold)
+						readset = readset.subset([i for i, read in enumerate(readset) if len(read) >= 2])
+						logger.info('Kept %d reads that cover at least two variants each', len(readset))
+						merged_reads = readset
+						if merge_reads_ :
+							merged_reads = merge_reads(readset, merge_reads_error_rate, merge_reads_max_error_rate, merge_reads_positive_threshold, merge_reads_negative_threshold)
+						selected_reads = select_reads(merged_reads, max_coverage_per_sample, preferred_source_ids = vcf_source_ids)
 
 					readsets[sample] = selected_reads
 					if (len(family) == 1) and not distrust_genotypes:
