68a6f562712d9008b47a8c1ba1e7bf51d56ffc83
Marcel Martin <marcel.martin@scilifelab.se>
2020-04-29 12:10:51 +0200

    Use a more recent buildwheels script

--- buildwheels.sh
+++ buildwheels.sh
@@ -5,21 +5,27 @@
 #
 # It is best to run this in a fresh clone of the repository!
 #
-# The wheels will be put into the wheelhouse/ subdirectory.
+# Run this within the repository root:
+#   ./buildwheels.sh
 #
-# For interactive tests:
-#   docker run -it -v $(pwd):/io quay.io/pypa/manylinux2010_x86_64 /bin/bash
+# The wheels will be put into the dist/ subdirectory.
 
 set -xeuo pipefail
 
-plat=manylinux2010_x86_64
+manylinux=quay.io/pypa/manylinux2010_x86_64
+platform=manylinux2010_x86_64
 
-# For convenience, if this script is called from outside a docker container,
+# For convenience, if this script is called from outside of a docker container,
 # it starts a container and runs itself inside of it.
 if ! grep -q docker /proc/1/cgroup; then
   # We are not inside a container
-  docker pull quay.io/pypa/${plat}
-  exec docker run --rm -v $(pwd):/io quay.io/pypa/${plat} /io/$0
+  docker pull ${manylinux}
+  exec docker run --rm -v $(pwd):/io ${manylinux} /io/$0
+fi
+
+if ! test -d /io/dist; then
+  mkdir /io/dist
+  chown --reference=/io/setup.py /io/dist
 fi
 
 # Strip binaries (copied from multibuild)
@@ -27,23 +33,16 @@ STRIP_FLAGS=${STRIP_FLAGS:-"-Wl,-strip-all"}
 export CFLAGS="${CFLAGS:-$STRIP_FLAGS}"
 export CXXFLAGS="${CXXFLAGS:-$STRIP_FLAGS}"
 
-# We support only Python 3.5+
-rm /opt/python/cp{27,34}*
-
-PYBINS="/opt/python/*/bin"
-HAS_CYTHON=0
-for PYBIN in ${PYBINS}; do
-#    ${PYBIN}/pip install -r /io/requirements.txt
+for PYBIN in /opt/python/cp3[5678]-*/bin; do
     ${PYBIN}/pip wheel --no-deps /io/ -w wheelhouse/
-    ls wheelhouse/
 done
+ls wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat ${plat} -w repaired/
+    auditwheel repair "$whl" --plat ${platform} -w repaired/
 done
 
 # Created files are owned by root, so fix permissions.
-mkdir -p /io/dist
-chown -R --reference=/io/setup.py repaired/ /io/dist/
+chown -R --reference=/io/setup.py repaired/
 mv repaired/*.whl /io/dist/
