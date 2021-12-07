560f9ed1ff340cab7c25d3450dfd3bc5c0a3def8
Samarendra <samarendrapani1998@gmail.com>
2021-12-07 15:16:00 +0530

    Debugging transitionprobabilitycomputer.cpp

--- src/transitionprobabilitycomputer.cpp
+++ src/transitionprobabilitycomputer.cpp
@@ -30,33 +30,39 @@ TransitionProbabilityComputer::TransitionProbabilityComputer(const unsigned int&
     vector<unsigned int> paths_j;
     vector<unsigned int> previous_references;
     unsigned int matrix_index;
-    unsigned int count;
+    unsigned int f_index;
     double value;
     vector<unsigned int>::iterator reference_i;
     vector<unsigned int>::iterator reference_j;
     
-    // Takes n_haplotype_path^4 to make the transition matrix. Have to work on decreasing this as it affects scalability.
+    // Iterating over the alleles present at the position for the first haplotype
     for (int i = 0; i < n_alleles; i++) {
+        // Extracting the paths which have the allele for the first haplotype
         paths_i = allele_to_reference[i];
+        // Iterating over the alleles present at the position for the second haplotype
         for (int j = 0; j < n_alleles; j++) {
+            // Extracting the paths which have the allele for the second haplotype
             paths_j = allele_to_reference[j];
             matrix_index = i*n_alleles + j;
             transition_probability_matrices[matrix_index].remake(pow(allele_reference.size(),2), paths_i.size()*paths_j.size(), 0.0);
-            for (unsigned int p_index = 0; p_index < pow(allele_reference.size(),2); p_index++) {
-                count = 0;
-                previous_references = column.index_to_reference_allele(p_index, 0);
-                for (reference_i = paths_i.begin(); reference_i < paths_i.end(); reference_i++) {
-                    for (reference_j = paths_j.begin(); reference_j < paths_j.end(); reference_j++) {
+            f_index = 0;
+            // Iterating over the nodes which contain the allele pair
+            for (reference_i = paths_i.begin(); reference_i < paths_i.end(); reference_i++) {
+                for (reference_j = paths_j.begin(); reference_j < paths_j.end(); reference_j++) {
+                    // Iterating over the indices in the previous column
+                    for (unsigned int p_index = 0; p_index < pow(allele_reference.size(),2); p_index++) {
+                        previous_references = column.index_to_reference_allele(p_index, 0);
                         if ((*reference_i == previous_references[0]) && (*reference_j == previous_references[1])) value = qr*qr;
                         if ((*reference_i != previous_references[0]) && (*reference_j == previous_references[1])) value = pr*qr;
                         if ((*reference_i == previous_references[0]) && (*reference_j != previous_references[1])) value = pr*qr;
                         if ((*reference_i != previous_references[0]) && (*reference_j != previous_references[1])) value = pr*pr;
-                        reordering_map[matrix_index].push_back(column.reference_allele_to_index(*reference_i, *reference_j));
-                        transition_probability_matrices[matrix_index].set(p_index, count, value);
-                        count++;
+                        transition_probability_matrices[matrix_index].set(p_index, f_index, value);
                     }
+                    reordering_map[matrix_index].push_back(column.reference_allele_to_index(*reference_i, *reference_j));
+                    f_index++;
                 }
             }
+            
         }
     }
 }
