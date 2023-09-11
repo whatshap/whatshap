38449be7f8d87f12eeeef5076c846eaf67c144b9
Hufsah-Ashraf <hafsah_ashraf@hotmail.com>
2023-09-12 01:07:27 +0200

    phred_scores.py

--- whatshap/phred_scores.py
+++ whatshap/phred_scores.py
@@ -8,56 +8,43 @@ import argparse
 import math
 from pathlib import Path
 import csv
+from collections import defaultdict
 
 
 def phred(inputfile, outputfile, epsilon_value, kmer_size):
-    probs = {}
+    counts = defaultdict(int)
+    seen_comb = defaultdict(int)
+    comb_count = defaultdict(int)
     probabilities = {}
-    seen_comb = {}
-    comb_count = {}
     k = int(kmer_size)
     total_comb = float(4**k)
     epsilon = float(epsilon_value)
     path = Path(inputfile)
     glob_path = path.glob("*.txt")
-    writer = open(outputfile, "w")
     # Iterate over each chromosome file
     for file in glob_path:
-        counts_file = open(file, "r")
-        reader = csv.reader(counts_file, delimiter="\t")
-        for line in reader:
-            ref = line[1]
-            read = line[2]
-            count = int(line[3])
-            new_key = (ref, read)
-            if new_key in probs:
-                probs[new_key] += count
-            else:
-                probs[new_key] = count
+        with open(file, "r") as counts_file:
+            reader = csv.reader(counts_file, delimiter="\t")
+            for line in reader:
+                ref = line[1]
+                read = line[2]
+                count = int(line[3])
+                counts[(ref, read)] += count
 
-    for every_key in probs:
-        if every_key[0] in seen_comb:
-            seen_comb[every_key[0]] += 1
-        else:
-            seen_comb[every_key[0]] = 1
-        if every_key[0] in comb_count:
-            comb_count[every_key[0]] += probs[every_key]
-        else:
-            comb_count[every_key[0]] = probs[every_key]
-
-    for s in probs:
-        probability = probs[s] / (comb_count[s[0]] + ((total_comb - seen_comb[s[0]]) * epsilon))
-        e_probability = epsilon / (comb_count[s[0]] + ((total_comb - seen_comb[s[0]]) * epsilon))
-        phred_score = -10 * math.log10(float(probability))
-        e_phred_score = -10 * math.log10(float(e_probability))
-        if s[0] in probabilities:
-            probabilities[s[0]].append("1")
-            writer.write(str(s[0]) + "\t" + str(s[1]) + "\t" + str(phred_score) + "\n")
-        else:
-            probabilities[s[0]] = list()
-            probabilities[s[0]].append("1")
-            writer.write(str(s[0]) + "\t" + str(s[1]) + "\t" + str(phred_score) + "\n")
-            writer.write(str(s[0]) + "\t" + "-5" + "\t" + str(e_phred_score) + "\n")
+    for every_key in counts:
+        seen_comb[every_key[0]] += 1
+        comb_count[every_key[0]] += counts[every_key]
+        
+    with open(outputfile, "w") as writer:
+        for s in counts:
+            probability = counts[s] / (comb_count[s[0]] + ((total_comb - seen_comb[s[0]]) * epsilon))
+            e_probability = epsilon / (comb_count[s[0]] + ((total_comb - seen_comb[s[0]]) * epsilon))
+            phred_score = -10 * math.log10(float(probability))
+            e_phred_score = -10 * math.log10(float(e_probability))
+            if s[0] not in probabilities:
+                probabilities[s[0]] = 1
+                print(s[0], -5, e_phred_score, sep="\t", file=writer)
+            print(s[0], s[1], phred_score, sep="\t", file=writer)
 
 
 if __name__ == "__main__":
