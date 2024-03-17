63e0b187e2ade75181736b5ce151d672320dff3d
Marcel Martin <marcel.martin@scilifelab.se>
2024-03-17 22:15:09 +0100

    Avoid renaming the imports

--- tests/test_kmeralign.py
+++ tests/test_kmeralign.py
@@ -1,9 +1,8 @@
 """
 Tests for kmer-alignment performed by kmerald
-
 """
-from whatshap.align import enumerate_all_kmers as enum
-from whatshap.align import kmer_align as k_align
+from whatshap.align import enumerate_all_kmers
+from whatshap.align import kmer_align
 
 
 non_enumerable = ["", "A", "AC", "ATCGC", "NNNNNNNNNNN", "BANANA", "MISSISSIPPI"]
@@ -30,23 +29,23 @@ SEQ_2 = ["GCCCAAA", "GGCCCAAA", "GCCCTTTAAA", "AAAA", ""]
 
 def test_enumeration():
     for string in non_enumerable:
-        assert len(enum(str(string).encode("UTF-8"), 9)) == 0
-        assert len(enum(str(string).encode("UTF-8"), 7)) == 0
-        assert len(enum(str(string).encode("UTF-8"), 6)) == 0
-    assert list(enum(str("TAAATCCTGG").encode("UTF-8"), 7)) == [12341, 215, 862, 3450]
-    assert list(enum(str("TAAATCCTGG").encode("UTF-8"), 11)) == []
+        assert len(enumerate_all_kmers(str(string).encode("UTF-8"), 9)) == 0
+        assert len(enumerate_all_kmers(str(string).encode("UTF-8"), 7)) == 0
+        assert len(enumerate_all_kmers(str(string).encode("UTF-8"), 6)) == 0
+    assert list(enumerate_all_kmers(str("TAAATCCTGG").encode("UTF-8"), 7)) == [12341, 215, 862, 3450]
+    assert list(enumerate_all_kmers(str("TAAATCCTGG").encode("UTF-8"), 11)) == []
 
 
 def test_kmeralign():
-    seq1 = enum(str("AATCCTGG").encode("UTF-8"), 5)
-    seq2 = enum(str("AATCCGGG").encode("UTF-8"), 5)
-    assert k_align(seq1, seq2, costs, 5) == 13
-    assert k_align(seq2, seq1, costs, 5) == 30
+    seq1 = enumerate_all_kmers(str("AATCCTGG").encode("UTF-8"), 5)
+    seq2 = enumerate_all_kmers(str("AATCCGGG").encode("UTF-8"), 5)
+    assert kmer_align(seq1, seq2, costs, 5) == 13
+    assert kmer_align(seq2, seq1, costs, 5) == 30
     for s1 in SEQ_1:
         for s2 in SEQ_2:
-            e_s1 = enum(s1.encode("UTF-8"), 5)
-            e_s2 = enum(s2.encode("UTF-8"), 5)
+            e_s1 = enumerate_all_kmers(s1.encode("UTF-8"), 5)
+            e_s2 = enumerate_all_kmers(s2.encode("UTF-8"), 5)
             if e_s1 != e_s2:
-                assert k_align(e_s1, e_s2, costs, gap) == gap * (len(e_s1) + len(e_s2))
+                assert kmer_align(e_s1, e_s2, costs, gap) == gap * (len(e_s1) + len(e_s2))
             else:
-                assert k_align(e_s1, e_s2, costs, gap) == 0
+                assert kmer_align(e_s1, e_s2, costs, gap) == 0
