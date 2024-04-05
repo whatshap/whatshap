3bd64086f4b54f8914e210ca25f5e3d38b725f04
Nikolai Karpov <nikolai.karpov@nanoporetech.com>
2024-04-05 22:33:49 +0100

    add test for compute_votes

--- tests/test_run_haplotagphase.py
+++ tests/test_run_haplotagphase.py
@@ -3,7 +3,8 @@ Tests for whatshap extend module
 
 """
 
-from whatshap.cli.haplotagphase import run_haplotagphase
+from whatshap.cli.haplotagphase import compute_votes, run_haplotagphase
+from whatshap.core import Read
 
 
 def test_extend():
@@ -13,3 +14,32 @@ def test_extend():
         reference="tests/data/pacbio/reference.fasta",
         output="/dev/null",
     )
+
+
+def test_compute_votes():
+    a = Read("a", 60, 0, 0, 0, "", 1, 1)
+    a.add_variant(1, 0, 30)
+    a.add_variant(2, 0, 10)
+    a.add_variant(3, 0, 50)
+    b = Read("b", 60, 0, 0, 0, "", 2, 1)
+    b.add_variant(1, 1, 20)
+    b.add_variant(2, 0, 30)
+    b.add_variant(3, 0, 90)
+    c = Read("c", 60, 0, 0, 0, "", 1, 2)
+    c.add_variant(1, 1, 20)
+    c.add_variant(3, 0, 10)
+    d = Read("d", 60, 0, 0, 0, "", 0, 2)
+    d.add_variant(1, 0, 30)
+    d.add_variant(2, 0, 10)
+    d.add_variant(3, 0, 50)
+    e = Read("d", 60, 0, 0, 0, "", 1, 0)
+    e.add_variant(1, 0, 30)
+    e.add_variant(2, 0, 10)
+    e.add_variant(3, 0, 50)
+    expected_votes = {
+        1: {(0, 0): 50, (0, 1): 0, (1, 1): 20, (1, 0): 0},
+        2: {(0, 0): 10, (0, 1): 30},
+    }
+    votes = compute_votes({1: False, 2: False, 3: True}, [a, b, c])
+    print(votes)
+    assert votes == expected_votes, f"Expected votes do not match: {votes}"
